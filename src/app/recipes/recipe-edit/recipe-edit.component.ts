c44094ecf161991ee7437af9978b1e1f773d5c1c
Farzan <far.ned.me@gmail.com>
2020-05-15 18:29:02 +0430

    Adding New Ingredient Controls

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -35,6 +35,15 @@ export class RecipeEditComponent implements OnInit {
     
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    );
+  }
+
   private initForm() {
     let recipeName: string = '';
     let recipeImagePath: string = '';
