705fdd460e2fb12fca514021982491da5671570f
Farzan <far.ned.me@gmail.com>
2020-05-15 20:06:44 +0430

    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataStorageService {
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+  ) { }
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+
+    return this.http.put('https://magic-recipe-42cee.firebaseio.com/recipes.json', recipes)
+      .subscribe(
+        response => {
+          
+        }
+      );
+  }
 }
